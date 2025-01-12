e44022a3e6dbc6a45b4addf264e87406ba5b261a
Sean Parkin <sdparkin@protonmail.com>
2025-01-12 17:45:15 -0600

    Created updatenote route

---
+++ src/routes/updatenote/[stopid]/+page.server.ts
@@ -0,0 +1,54 @@
+import db from '$lib/db';
+import { ObjectId } from 'mongodb';
+import type { PageServerLoad } from './$types';
+import { fail, redirect, type Actions } from '@sveltejs/kit';
+
+export const load: PageServerLoad = async (event) => {
+  const { stopid } = event.params;
+  const response = await db.collection('notes').findOne({ _id: new ObjectId(stopid) });
+  const stop = JSON.stringify(response);
+
+  return {
+    stop
+  };
+};
+
+export const actions: Actions = {
+  updateNote: async ({ request }) => {
+    // Get all of the data from the form
+    const formData = await request.formData();
+    let stopId = formData.get('stopId') as string;
+    const custName = formData.get('custName') as string;
+    const address = formData.get('address')  as string;
+    const suite = formData.get('suite') as string;
+    const city = formData.get('city') as string;
+    const deliveryLocation = formData.get('deliveryLocation') as string;
+    const notes = formData.get('notes') as string;
+    const signers = formData.get('signers') as unknown as Array<string>;
+
+    // Remove double quotes from string
+    stopId = stopId.replace(/"/g, "")
+
+    // Update the stop
+    const updateStop = await db.collection('notes').updateOne(
+      { _id: new ObjectId(stopId) },
+      {
+        $set: {
+          custName: custName,
+          address: address,
+          suite: suite,
+          city: city,
+          deliveryLocation: deliveryLocation,
+          notes: notes,
+          signers: signers
+        }
+      }
+    );
+
+    if (updateStop.modifiedCount !== 1) {
+      return fail(400, { message: 'Stop was not updated' });
+    }
+
+    throw redirect(302, '/stoplist');
+  }
+};
---
+++ src/routes/updatenote/[stopid]/+page.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import NavBar from '../../../components/NavBar.svelte';
+	import { enhance } from '$app/forms';
+
+	let { data } = $props();
+	let stop = $state(JSON.parse(data.stop));
+</script>
+
+<div class="main">
+	<NavBar />
+	<h1 class="updateTitle">Update Stop</h1>
+	<p>{stop.error}</p>
+	<form
+		class="updateForm"
+		use:enhance={({ formData }) => formData.append('stopId', JSON.stringify(stop._id))}
+		action="?/updateNote"
+		method="POST"
+	>
+		<input name="custName" type="text" placeholder="Customer Name" bind:value={stop.custName} />
+		<input name="address" type="text" placeholder="Address" bind:value={stop.address} />
+		<input name="suite" type="text" placeholder="Suite" bind:value={stop.suite} />
+		<input name="city" type="text" placeholder="City" bind:value={stop.city} />
+		<input
+			name="deliveryLocation"
+			type="text"
+			placeholder="Delivery Location"
+			bind:value={stop.deliveryLocation}
+		/>
+		<input name="notes" type="text" placeholder="Notes" bind:value={stop.notes} />
+		<input name="signers" type="text" placeholder="Signers" bind:value={stop.signers} />
+		<!-- TODO: Add "handleSubmit" for line below -->
+		<input type="submit" value="Update Note" />
+	</form>
+</div>
+
+<style>
+	.main {
+		background: #c0c6c8;
+	}
+
+	.updateTitle {
+		font-size: 3rem;
+		color: red;
+		text-align: center;
+	}
+
+	.updateForm {
+		align-items: center;
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+	}
+	input[type='text'] {
+		margin: 0.5rem 0;
+		padding: 1rem 0.5rem;
+		width: 40%;
+		border-radius: 8px;
+		border: solid 1px #c0c6c8;
+		box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+	}
+
+	input[type='submit'] {
+		background: #767676;
+		width: 6rem;
+		padding: 1rem 0.5rem;
+		margin-top: 0.5rem;
+		border: solid 1px #767676;
+		border-radius: 8px;
+		box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+	}
+</style>
