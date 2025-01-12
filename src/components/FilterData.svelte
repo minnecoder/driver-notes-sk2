aa6b7cc87532f74421aa89299a2a5c79adc3319a
Sean Parkin <sdparkin@protonmail.com>
2025-01-12 17:39:52 -0600

    Created FilterData component

---
+++ src/components/FilterData.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let intialStops = [];
+
+	function filterList() {
+		const stops = initialStops;
+		console.log(stops);
+		setStops(stops);
+	}
+</script>
+
+<div>
+	<form>
+		<input type="text" placeholder="Search" onChange={this.filterList} />
+	</form>
+
+	<div>
+		{this.state.stops.map((stop) => <SingleStop key={stop.id} stop={stop} />)}
+	</div>
+</div>
